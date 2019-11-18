5443eaad343da6417edc02d04efb93a311efc18d
LilyLiu0719 <lily.ylliu@gmail.com>
2019-11-18 22:59:25 +0800

    27.3

--- competition1/competition1.m
+++ competition1/competition1.m
@@ -1,27 +1,26 @@
 %% Setup the plant
 s = tf('s');
 P = -1.202*(s-1)/(s*(s+9)*(s^2+12*s+56.25));
-figure;
-subplot(2,1,1);
-step(P,2);
-title('Open-loop step response');
+% figure; 
+% subplot(2,1,1);
+% step(P,2);
+% title('Open-loop step response');
 
 %% Design the PID controller
-% Kp = 10;
-% Ki = 10;
-% Kd = 0;
-% C = pid(Kp,Ki,Kd);
-C = -(s*(s+9)*(s^2+12*s+56.25))/(1.202*(s-1)*(s^2+100*s+10000));
+Kp = 220;
+Ki = 0;
+Kd = 77;
+C = pid(Kp,Ki,Kd);
+% C = -(s*(s+9)*(s^2+12*s+56.25))/(1.202*(s-1)*(s^2+100*s+10000));
 H = feedback(C*P,1);
 S = stepinfo(H);
 ts = S.SettlingTime;
 tr = S.RiseTime;
 Mo = S.SettlingMax;
-ess = 0;
 
 %% Simulate the closed-loop step response
-figure;
+figure; 
 subplot(2,1,2);
-step(H,2);
+step(H,10);
 title('Closed-loop step response');
-J = 10*tr + ts + 20*Mo + 100*ess;
+J = 10*tr + ts + 20*Mo;
